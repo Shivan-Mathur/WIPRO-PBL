0be8457c6603f64ea4e4e00f25764c9e43354fcb
Shivan-Mathur <31898751+Shivan-Mathur@users.noreply.github.com>
2020-04-25 12:55:19 +0530

    Add files via upload

---
+++ Java Fundamentals/Language Basics/Sum.java
@@ -0,0 +1,11 @@
+package Java_Fundamentals;
+
+public class Sum {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("The sum of " + args[0] + " and "+ args[1] + " is "+ (args[0] + args[1]) );
+	}
+
+}
---
+++ Java Fundamentals/Language Basics/Welcome.java
@@ -0,0 +1,12 @@
+package Java_Fundamentals;
+
+public class Welcome {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Welcome " + args[0]);
+
+	}
+
+}
